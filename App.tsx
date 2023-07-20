56381b83b20814d7e73c39a98f7670d7f1113484
MartianLee <martionlee@gmail.com>
2023-07-20 10:58:47 +0900

    Install react-native-paper, react-navigation and implement sample component

--- App.tsx
+++ App.tsx
@@ -1,118 +1,148 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import * as React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
   Text,
-  useColorScheme,
+} from 'react-native-paper';
+import App from './src/Home';
+import NewCard from './src/components/NewCard';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <SafeAreaView>
+      <ScrollView style={styles.sectionContainer}>
+        <Text variant="displayLarge">Home</Text>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
+          <NewCard key={i} />
+        ))}
+      </ScrollView>
+      <TouchableOpacity
+        onPress={() => console.log('Pressed!')}
+        style={styles.writeButton}>
+        <Text variant="headlineSmall">Write</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+function OtherScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text variant="displayLarge">My Page</Text>
     </View>
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const BottomTabNavigator = () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <Tab.Navigator
+      backBehavior={'none'}
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === '홈') {
+            // iconName = focused
+            //   ? 'ios-information-circle'
+            //   : 'ios-information-circle-outline';
+            iconName = 'home';
+            return (
+              <Image
+                source={require('./assets/icon/home.png')}
+                style={{width: size, height: size, backgroundColor: color}}
+              />
+            );
+          } else if (route.name === '마이페이지') {
+            // iconName = focused ? 'ios-list' : 'ios-list-outline';
+            iconName = 'mypage';
+            return (
+              <Image
+                source={require('./assets/icon/mypage.png')}
+                style={{width: size, height: size, backgroundColor: color}}
+              />
+            );
+          }
+          // You can return any component that you like here!
+          // return <Ionicons name={iconName} size={size} color={color} />;
+          console.log(color, size, route.name);
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'white',
+        tabBarLabel(props) {
+          return <Text>{props.children}</Text>;
+        },
+      })}>
+      <Tab.Screen
+        name={'홈'}
+        component={HomeScreen}
+        options={{headerShown: false}}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+      <Tab.Screen name={'마이페이지'} component={OtherScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={'Home'}>
+          <Stack.Screen
+            name="Home"
+            component={BottomTabNavigator}
+            options={{headerShown: false}}
+          />
+          {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        </Stack.Navigator>
+        {/* <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+          <Stack.Navigator>
+            <Stack.Screen name="Other" component={OtherScreen} />
+          </Stack.Navigator> */}
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    marginTop: 16,
+    paddingHorizontal: 16,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  writeButton: {
+    position: 'absolute',
+    backgroundColor: 'tomato',
+    borderRadius: 30,
+    width: 100,
+    right: 20,
+    bottom: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
   },
 });
-
-export default App;
