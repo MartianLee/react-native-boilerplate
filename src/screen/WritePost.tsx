43fa8f4c385796054f4375279b1a9b3d0e292326
MartianLee <martionlee@gmail.com>
2023-08-08 18:14:53 +0900

    Implement image filter and share after photo captured

--- src/screen/WritePost.tsx
+++ src/screen/WritePost.tsx
@@ -8,11 +8,15 @@ import {
   StyleSheet,
   TouchableOpacity,
   View,
+  Platform,
 } from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import NewCard from '../components/NewCard';
 import {Camera, PhotoFile, useCameraDevices} from 'react-native-vision-camera';
 import RNFS from 'react-native-fs';
+import ViewShot from 'react-native-view-shot';
+import {useEffect, useRef} from 'react';
+import Share from 'react-native-share';
 
 type WritePostScreenProps = {
   navigation: any;
@@ -22,11 +26,33 @@ function WritePostScreen(props: WritePostScreenProps) {
 
   const devices = useCameraDevices();
   const device = devices.back;
-  const camera = React.useRef<Camera>(null);
+  const camera = useRef<Camera>(null);
+  const viewShotRef = useRef<ViewShot>(null);
   // const [capturedPhoto, setCapturedPhoto] = React.useState<ImageSourcePropType>(null);
   const [capturedPhoto, setCapturedPhoto] = React.useState<{
     uri: string;
   } | null>(null);
+
+  const handleOverlayAndSave = async () => {
+    if (viewShotRef.current) {
+      // Capture the combined view
+      viewShotRef.current
+        .capture?.()
+        .then(async (uri: string) => {
+          // Save the captured view to a new file
+          const savePath = RNFS.DocumentDirectoryPath + '/combined_image.jpg';
+          await RNFS.copyFile(uri, savePath);
+          const result = await Share.open({
+            url: Platform.OS === 'ios' ? `file://${uri}` : uri,
+          });
+          console.log('Combined image saved to: ', savePath);
+          // setImageUri(savePath);
+        })
+        .catch((error: any) => {
+          console.warn('Error capturing combined image: ', error);
+        });
+    }
+  };
 
   const takePhoto = async () => {
     if (camera.current) {
@@ -34,6 +60,7 @@ function WritePostScreen(props: WritePostScreenProps) {
         flash: 'off',
       });
       setCapturedPhoto({uri: 'file://' + photo.path});
+      // handleOverlayAndSave();
       // const savePath = RNFS.DocumentDirectoryPath + '/my_photo.jpg';
       // await RNFS.moveFile(photo.path, savePath);
       // setCapturedPhoto({uri: savePath});
@@ -65,11 +92,20 @@ function WritePostScreen(props: WritePostScreenProps) {
       case 'denied':
         // 권한 요청을 했지만 거부됐을때 실행할 로직
         // ex) 설정으로 이동, 권한이 없으면 카메라 실행할 수 없다는 알림창 등등
-        await Linking.openSettings();
+        const newCameraPermission2 = await Camera.requestCameraPermission();
+        if (newCameraPermission2 !== 'authorized') {
+          await Linking.openSettings();
+        }
         break;
     }
   };
-  React.useEffect(() => {
+
+  // const onImageLoad = () => {
+  //   console.log(`Image Loaded: ${capturedPhoto?.uri}`);
+  //   handleOverlayAndSave();
+  // };
+
+  useEffect(() => {
     requestCameraPermission();
   }, []);
 
@@ -89,16 +125,7 @@ function WritePostScreen(props: WritePostScreenProps) {
         contentContainerStyle={{
           flexGrow: 1,
         }}>
-        <Text variant="displayLarge">WritePost</Text>
-        <View style={{flex: 1}}>
-          {capturedPhoto ? (
-            <Image source={{uri: capturedPhoto?.uri}} style={{height: 200}} />
-          ) : (
-            <View style={{height: 200}}>
-              <Text>No photo</Text>
-            </View>
-          )}
-        </View>
+        <Text variant="displaySmall">Challenge</Text>
         <Camera
           ref={camera}
           style={styles.cameraPreview}
@@ -106,6 +133,36 @@ function WritePostScreen(props: WritePostScreenProps) {
           photo={true}
           isActive={true}
         />
+        <View style={{flex: 1}}>
+          {capturedPhoto ? (
+            <ViewShot
+              ref={viewShotRef}
+              options={{
+                fileName: 'test',
+                format: 'jpg',
+                quality: 1,
+              }}
+              style={{
+                backgroundColor: 'transparent',
+                flex: 1,
+              }}>
+              <Image
+                source={require('../../assets/image/gold-tier.png')}
+                style={{
+                  position: 'absolute',
+                  zIndex: 100,
+                  height: 600,
+                  backgroundColor: 'transparent',
+                }}
+              />
+              <Image source={{uri: capturedPhoto?.uri}} style={{height: 600}} />
+            </ViewShot>
+          ) : (
+            <View style={{height: 200}}>
+              <Text>No photo</Text>
+            </View>
+          )}
+        </View>
       </ScrollView>
       <TouchableOpacity
         onPress={
@@ -114,6 +171,11 @@ function WritePostScreen(props: WritePostScreenProps) {
         }
         style={styles.writeButton}>
         <Text variant="headlineSmall">Take Photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleOverlayAndSave}
+        style={styles.shareButton}>
+        <Text variant="headlineSmall">Share Photo</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -135,7 +197,18 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     width: 100,
     right: 20,
-    bottom: 20,
+    bottom: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  shareButton: {
+    position: 'absolute',
+    backgroundColor: 'tomato',
+    borderRadius: 30,
+    width: 100,
+    right: 20,
+    bottom: 110,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 10,
